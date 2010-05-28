51e06dc45a878fd3a33d37c96118b183442d9ecb
Aaron <aaron@porpoiseltd.com>
2010-05-28 18:29:06 +0100

    more test cases

--- src/test/java/com/porpoise/ga/countdown/CountdownTest.java
+++ src/test/java/com/porpoise/ga/countdown/CountdownTest.java
@@ -29,8 +29,11 @@ public class CountdownTest
     public static Collection<Object[]> getTestArgs()
     {
         return Arrays.asList(//
-                new Object[] { 13, 5, 3, 2, 1 },//        
-                new Object[] { 24, 16, 8, 7, 9 }//        
+                new Object[] { 13, 5, 3, 2, 1 },// 13 = 2 X 5 / 1 + 3
+                new Object[] { 24, 16, 8, 7, 9 },// 24 = 16 - 8 + 9 + 7
+                new Object[] { 4, 3, 6, 2, 12 },// 4 = 12 - 6 / 3 X 2
+                new Object[] { 18, 7, 13, 4, 2 },// 18 = 2 - 4 + 7 + 13
+                new Object[] { 12, 5, 2, 7, 8 } // 12 = 7 + 8 - 5 + 2
                 );
     }
 
